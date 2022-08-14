fba718228d7a9e1d24a66d7c4454052b221b3bd9
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-14 21:49:02 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,13 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
+    public static void main(String[] args) {
+        repeatIsBad();
+    }
 
-    @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-
+    public static void repeatIsBad() {
+        for (int runner = 0; runner < 20; ++runner) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
     }
 }
